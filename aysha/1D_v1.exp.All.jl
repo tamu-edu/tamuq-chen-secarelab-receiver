3297857a6849e520936c84e4bb8c99e72dea0965
kkakosim@IndigoL7i <kkakosim@gmail.com>
2024-11-22 17:27:40 +0100

    before changing ρf to f(T) everywhere
    o cleaned up geometry
    + temperature depended ρf
    - missing Nu and h calculations as a function of T, because of ρf(T)

--- aysha/1D_v1.exp.All.jl
+++ aysha/1D_v1.exp.All.jl
@@ -12,59 +12,82 @@ begin #libraries
 
 end
 
-begin #define parameters
-    L = 137e-3 #m
+begin #FIXED Parameters
+
     #α = keff/ρ*Cp #kW/m2.K 
     Tamb = (22.448 + 273.15) #K (same for all exp) 
-    deltax = 0.002795918367346939 #discretization (m)
-    w_t = 19 / 1000 #m
-    A_t = w_t * w_t #m2 - for the whole receiver (19x19mm2)
-    #A_st = 176e-6 #total area - solid m2
-    #A_ft = 49e-6 # total area - fluid m2
-    w_t = 19.e-3 #m
-    A_frt = w_t * w_t #m2 - for the whole receiver (19x19mm2)
+    #deltax = 0.002795918367346939 #discretization (m)
+    # w_t = 19 / 1000 #m
+    # A_t = w_t * w_t #m2 - for the whole receiver (19x19mm2)
+    # #A_st = 176e-6 #total area - solid m2
+    # #A_ft = 49e-6 # total area - fluid m2
+    # w_t = 19.e-3 #m
+    # A_frt = w_t * w_t #m2 - for the whole receiver (19x19mm2)
+    # w_chnl = 1.5e-3 #m
+    # A_chnl_p = w_chnl * L * 4  #m2 channel periphery
+    # A_chnl_frt = w_chnl * w_chnl #m2 channel front
+    # n_chnl = 10 * 10
+    # A_exchange = A_chnl_p * n_chnl #m2 - contact area between fluid and solid
+    # A_chnl_frt_all = A_chnl_frt * n_chnl #m2 all frontal area of channels
+    # A_frt_solid = A_frt - A_chnl_frt_all #m2 - frontal area of solid
+    # Vs = A_frt * L #m3
+    # Vf = n_chnl * A_chnl_frt * L
+
+    # Receiver dimensions
+    w_t = 19.e-3  # Width of the receiver in meters
+    A_frt = w_t * w_t  # Total frontal area of the receiver (m^2)
+    L = 137e-3 #m Length
     A_s_p = w_t * L * 4 #total area solid periphery m2
-    w_chnl = 1.5e-3 #m
-    A_chnl_p = w_chnl * L * 4  #m2 channel periphery
-    A_chnl_frt = w_chnl * w_chnl #m2 channel front
-    n_chnl = 10 * 10
-    A_exchange = A_chnl_p * n_chnl #m2 - contact area between fluid and solid
-    A_chnl_frt_all = A_chnl_frt * n_chnl #m2 all frontal area of channels
-    A_frt_solid = A_frt - A_chnl_frt_all #m2 - frontal area of solid
-    Vs = A_frt * L #m3
-    Vf = n_chnl * A_chnl_frt * L
-    qlpm = 7.12 #lpm
-    q = qlpm / (1000 * 60) #m3/s
-    ρ = 1.2 #kg/m3 - density of air at lab conditions
-    m = q * ρ #kg/s
-    #V= q/(A_channel*n_channel) #m/s - using the area of the whole receiver (18x18mm2)
-    V = 0.57 #m/s (calculated from excel sheet and COMSOL)
-    th_s = 0.4e-3 #m
-    th_f = 0.7e-3 #m
+
+    # Channel dimensions
+    w_chnl = 1.5e-3  # Width of a single channel (m)
+    A_chnl_frt = w_chnl * w_chnl  # Frontal area of a single channel (m^2)
+    A_chnl_p = w_chnl * L * 4  # Periphery area of a single channel (m^2)
+    Lc = 4 * (w_t * w_t) / (4 * w_t) #hydraulic receiver diameter
+
+    # Channel array
+    n_chnl = 10 * 10  # Number of channels (10x10 array)
+    A_chnl_frt_all = A_chnl_frt * n_chnl  # Total frontal area of all channels (m^2)
+
+    # Solid geometrical calculations
+    A_frt_solid = A_frt - A_chnl_frt_all  # Frontal area of solid (m^2)
+    A_exchange = A_chnl_p * n_chnl  # Total contact area between fluid and solid (m^2)
+
+    # Volume calculations
+    Vs = A_frt_solid * L  # Corrected solid volume (m^3)
+    Vf = n_chnl * A_chnl_frt * L  # Fluid volume in the channels (m^3)
+
+
+    # #V= q/(A_channel*n_channel) #m/s - using the area of the whole receiver (18x18mm2)
+    # V = 0.57 #m/s (calculated from excel sheet and COMSOL)
+    # th_s = 0.4e-3 #m
+    # th_f = 0.7e-3 #m
     #Qv= I0*exp(-1000*x)  #kW/m2 - (K extinction coefficient taken from Howell and Hendrick paper pg.86, measure pore diameter and fix)
-    ϵ = 0.9 #absroptivity/emissivity
+
+    #Af = n_chnl * A_chnl_frt #m2
+    # Lc = 4 * (w_t * w_t) / (4 * w_t)
+    # w = 1.5e-3 #width of channel (m)
+    # Vi = w * w * n_chnl * L #m3
+    # Av = 4 * (w * L) / (w^2 * L) #specific area (m-1)
+    
+    #Constants
     σ = 5.17e-8 #W/m2.K^4 Stefan-Boltzmann constant
-    Lc = 4 * (w_t * w_t) / (4 * w_t)
-    kf = 0.056 #W/m.K
-    ρf = 0.5 #kg/m3
-    Cpf = 1090 #J/kg.K
-    mu = 2.0921e-5 #Pa.s
+    hext = 0 #10 #W/m2.K convective heat transfer coefficient for the front --> with forced flow assume that there is no such loss
+
+    
+    #SiC Properties
+    ϵ = 0.9 #absroptivity/emissivity
     e = 0.425 #porosity
-    Af = n_chnl * A_chnl_frt #m2
-    Lc = 4 * (w_t * w_t) / (4 * w_t)
-    w = 1.5e-3 #width of channel (m)
-    Vi = w * w * n_chnl * L #m3
-    Av = 4 * (w * L) / (w^2 * L) #specific area (m-1)
-    hext = 10 #W/m2.K
-    kins = 0.078 #W/m*K
+    
+    #parameters for the insulation
     r0 = 23 / 1000 #m
     r_ins = 42 / 1000 #m
-    r_H = 4 * (w_t * w_t) / (4 * w_t) #hydraulic receiver diameter
-    aCp = 1.
-end;
-#for interpolations 
-#1. extract T2 data
-#Exp 71 
+    kins = 0. #0.078/10 #W/m*K -->assume very few losses through the insulation
+end; 
+# #for interpolations 
+# #1. extract T2 data
+
+#Exp 71 One typical Insulation Temperature Profile
 begin
     D3 = XLSX.readxlsx("./SolarSimulator/EXCEL/Data_FPT0071_T2.xlsx")["Sheet 1 - Data_FPT0071_T2"]["A3:D7087"]
     y1d3_data = D3[:, 2] .+ 273.0 #T2 (insulation)
@@ -74,17 +97,16 @@ begin
     Tins_f(t) = Tins(t)
     @register_symbolic Tins_f(t)
 end
-begin
-    x11 = 0.0001:0.001383838383838384:0.137 #T2 (insulation)
-    Re_f(qlpm) = (ρf * (qlpm / 60 / 1000 / Af) * w_t) / mu
-    Re = (ρ * V * w) / mu
-    Pr = (Cpf * mu) / kf
-    Gz = (1 ./ x11) * Re_f(qlpm) * Pr * w
-    #2.create interpolation function
-    Gz_ = LinearInterpolation(x11, Gz)
-    Gz_f(x) = Gz_(x)
-    @register_symbolic Gz_f(x)
-end
+# begin
+#     x11 = 0.0001:0.001383838383838384:0.137 #T2 (insulation)
+#     Re_f(qlpm) = (ρf * (qlpm / 60 / 1000 / Af) * w_t) / mu
+#     Re = (ρ * V * w) / mu
+#     Gz = (1 ./ x11) * Re_f(qlpm) * Pr * w
+#     #2.create interpolation function
+#     Gz_ = LinearInterpolation(x11, Gz)
+#     Gz_f(x) = Gz_(x)
+#     @register_symbolic Gz_f(x)
+# end
 
 begin
     # Parameters, variables, and derivatives for system 1
@@ -96,11 +118,21 @@ begin
     Dx = Differential(x)
     Dxx = Differential(x)^2
    
+    ### Fluid Properties
+    ρf_f(x) = 352.716 * x^-1 #COMSOL
+    @register_symbolic ρf_f(x)
+    ρf = ρf_f(1000.) #kg/m3
+    m(qlpm) = ρf * (qlpm / 60 / 1000) #mass at the flowmeter conditions
+
+    mu = 2.0921e-5 #Pa.s
+    kf = 0.056 #W/m.K
     kf_f(x) = -0.00227583562 + 1.15480022e-4 * x^1 - 7.90252856e-8 * x^2 + 4.11702505e-11 * x^3 - 7.43864331e-15 * x^4 #COMSOL
     @register_symbolic kf_f(x)  
+    Cpf = 1090 #J/kg.K
     cpf_f(x) = 1.93e-10 * x^4 - 8.0e-7 * x^3 + 1.14e-3 * x^2 - 4.49e-1 * x + 1.06e3
     @register_symbolic cpf_f(x)
       
+    #Solid Properties
     #ks = (500.0) * 1.97 * ((1 - e)^1.5)/10. #W/m.K
     ρs = 3200  #kg/m3
     ks_f(x) = 191.9216 - 0.3261784 * x^1 + 2.739462e-4 * x^2 - 7.70926e-8 * x^3 #COMSOL SiC Alpha Polycrystaline
@@ -109,19 +141,14 @@ begin
     #Cps = 1290  #J/kg*K
     Cps(x) = 8.5e-5 * x^2 + 5.63e-2 * x - 4.05e7 * x^-2 + 1125.8 #J/kg*K
     @register_symbolic Cps(x)
+    aCp = 1. #correction factor
+
     #Nu = A*(1+(B*((Gz_f(x))^n)*exp(-C/Gz_f(x))))
     #Nu = A*(Re^B)*(Pr^C)
     #Cps(Ts) = (0.27+0.135e-4*(Ts)-9720*((Ts)^-2)+0.204e-7*((Ts)^2))/1000 #kJ/kg*K from manufacturer data
-    #p_opt = [A => 2., B => 0.5, n=> 0.5, C=> 20.]
-    p_opt = [A => 1000.0, B => 0.6, C => 7.0, aloss => 1.0]
-    p_opt = [A => 636.0, B => 0.44, C => 8.488, aloss => 10.]
-    p_cond = [Io => 456000.0, qlpm => 15.27]
-    p_math = Dict(vcat(p_opt, p_cond))
-    #extract the parameters names from p_math
-    p_names = [i for i in keys(p_math)]
-    #h_average = hfa * (qlpm^hfn)
-    #h_average = (Nu * kf) / Lc
-    Re_f(qlpm) = (ρ * (qlpm / 60 / 1000 / Af) * w) / mu
+
+    Re_f(qlpm) = (ρf * (qlpm / 60 / 1000 / A_chnl_frt_all) * Lc) / mu #density at ambient conditions ???
+    Pr = (Cpf * mu) / kf
     Nu_f(qlpm) = A * (Re_f(qlpm)^B) * (Pr^C)
     #Nu_f(qlpm) = A*(1+(B*((Gz_f(x))^n)*exp(-C/Gz_f(x))))
     h_avg_f(qlpm) = (Nu_f(qlpm) * kf) / Lc
@@ -133,11 +160,17 @@ begin
     t_max = 7084.0
 
     nc1 = 100
-
     x_num1 = range(x_min1, x_max1, length=nc1)
-
     dx = (x_max1 - x_min1) / (nc1 - 1)
 
+    ## SETUP Parameters
+
+    #p_opt = [A => 2., B => 0.5, n=> 0.5, C=> 20.]
+    p_opt = [A => 1000.0, B => 0.6, C => 7.0, aloss => 1.0]
+    p_cond = [Io => 456000.0, qlpm => 15.27]
+    p_math = Dict(vcat(p_opt, p_cond))
+    #extract the parameters names from p_math
+    p_names = [i for i in keys(p_math)]
 
     # PDE equation for system 1
 
@@ -146,16 +179,16 @@ begin
     #     Vf * ρf * Cpf * Dt(Tf(t, x)) ~ Vf * kf * Dxx(Tf(t, x)) - Vf * ρf * Cpf * V * Dx(Tf(t, x)) + (h_average) * Av * Vi * ((Ts(t, x) - Tf(t, x)))
     # ]
     eq1 = [
-        (1 - e) * Vs * Dt(Ts(t, x)) ~ (A_frt * ks_f(Ts(t,x)) * Dxx(Ts(t, x)) - (h_avg_f(qlpm) * A_exchange * ((Ts(t, x)) - Tf(t, x))) .- 0.1 * (kins * (r_ins / r0) .* (Ts(t, x) .- Tins_f(t)) * A_s_p / (r_ins - r0)))/(aloss * ρs * Cps(Ts(t,x))),
-        e * Vf * Dt(Tf(t, x)) ~ (Af * e * kf_f(Tf(t,x)) * Dxx(Tf(t, x)) - m * cpf_f(Tf(t,x)) * Dx(Tf(t, x)) + (h_avg_f(qlpm) * A_exchange * ((Ts(t, x)) - Tf(t, x))))/(ρf * cpf_f(Tf(t,x)))
+        Vs * Dt(Ts(t, x)) ~ (A_frt * ks_f(Ts(t,x)) * Dxx(Ts(t, x)) - (h_avg_f(qlpm) * A_exchange * ((Ts(t, x)) - Tf(t, x))) .- (kins * (r_ins / r0) .* (Ts(t, x) .- Tins_f(t)) * A_s_p / (r_ins - r0)))/(aloss * ρs * Cps(Ts(t,x))),
+        Vf * Dt(Tf(t, x)) ~ (A_chnl_frt_all * e * kf_f(Tf(t,x)) * Dxx(Tf(t, x)) - m(qlpm) * cpf_f(Tf(t,x)) * Dx(Tf(t, x)) + (h_avg_f(qlpm) * A_exchange * ((Ts(t, x)) - Tf(t, x))))/(ρf_f(Tf(t,x)) * cpf_f(Tf(t,x)))
     ]
     bcs1 = [
         Ts(0.0, x) ~ Tamb, # initial
         Tf(0.0, x) ~ Tamb, # initial
-        -A_frt * ks_f(Ts(t,x)) * Dx(Ts(t, x_max1)) ~ 0.0, # far right
-        -A_frt * ks_f(Ts(t,x)) * Dx(Ts(t, x_min1)) ~ ϵ * Io * A_chnl_frt_all - ϵ * σ * A_chnl_frt_all * (Ts(t, x_min1)^4 - Tamb^4) - hext * A_chnl_frt_all * (Ts(t, x_min1) - Tamb),  # far left
-        -Af * kf_f(Tf(t,x)) * Dx(Tf(t, x_max1)) ~ 0.0, #-ρf * Cpf * V * A_ft * (Tf(t, x_max1) - Tamb), # exiting fluid
-        -Af * kf_f(Tf(t,x)) * Dx(Tf(t, x_min1)) ~ m * Cpf * (Tf(t, x_min1) - Tamb) # entering fluid (upstream temperature)
+        -A_frt_solid * ks_f(Ts(t,x)) * Dx(Ts(t, x_max1)) ~ 0.0, # far right
+        -A_frt_solid * ks_f(Ts(t,x)) * Dx(Ts(t, x_min1)) ~ ϵ * Io * A_frt - ϵ * σ * A_chnl_frt_all * (Ts(t, x_min1)^4 - Tamb^4) - hext * A_chnl_frt_all * (Ts(t, x_min1) - Tamb),  # far left
+        -A_chnl_frt_all * kf_f(Tf(t,x)) * Dx(Tf(t, x_max1)) ~ 0.0, #-ρf * Cpf * V * A_ft * (Tf(t, x_max1) - Tamb), # exiting fluid
+        -A_chnl_frt_all * kf_f(Tf(t,x)) * Dx(Tf(t, x_min1)) ~ m(qlpm) * cpf_f(Tf(t,x)) * (Tf(t, x_min1) - Tamb) # entering fluid (upstream temperature)
     ]
     # Space and time domain for system 1
     domains1 = [t ∈ Interval(t_min, t_max),
@@ -174,7 +207,7 @@ begin
 
 end
 
-sol1 = solve(prob, FBDF(), saveat=2)
+sol1 = solve(prob, FBDF(), saveat=2, reltol = 1e-7)
 begin
     Ts_front_t = sol1.u[(Ts(t, x))][:, 1]
     Tf_front_t = sol1.u[(Tf(t, x))][:, 2]
@@ -264,9 +297,6 @@ function remakeAysha(pguess_l, cond_k, time_opt; tolr=1e-7)
     temp_T = NLmodeloptim(time_opt, rmp, tolr)
     return temp_T
 end
-#the first entry in the simulation_conditions
-pguess = p_opt
-
 function lossAll(pguess_l, _)
 
     #to place the conditions loop
@@ -293,6 +323,8 @@ function lossAll(pguess_l, _)
     end
     return sum(lossr) #MSE
 end
+
+p_opt = [A => 636.0, B => 0.44, C => 8.488, aloss => 10.]  
 p0 = [x[2] for x in p_opt]
 optf = OptimizationFunction(lossAll, Optimization.AutoForwardDiff())
 #p_opt = [aCp => 1., hfa => 8., hfn =>0.66, aIo => 1.] 
@@ -357,7 +389,7 @@ begin #TI-8
         bar_width=0.4, xticks=(1:length(ordered_sim_conditions), ordered_sim_conditions), 
         bar_position=:dodge, color=[color_model color_exp], ylimit=(0, 1250))
 
-    plot1 = qqplot(T_mod_values, T_exp_values, label="", 
+    plot1 = qqplot(T_mod_values, T_exp_values, label="",
         xlabel="T_mod", ylabel="T_exp",ylims=(500, 800),xlims=(500, 800), aspect_ratio = :equal)
 end
 
